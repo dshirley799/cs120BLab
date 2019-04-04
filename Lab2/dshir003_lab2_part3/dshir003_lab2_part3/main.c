6e5e24cff5e3254d1b76cb7264b50b6b3f709587
dshirley799 <33436533+dshirley799@users.noreply.github.com>
2019-04-04 11:52:02 -0700

    Lab2 Part3

---
+++ Lab2/dshir003_lab2_part3/dshir003_lab2_part3/main.c
@@ -0,0 +1,18 @@
+/*
+ * dshir003_lab2_part3.c
+ *
+ * Created: 4/4/2019 11:47:29 AM
+ * Author : dspha
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
